3bcbdfe497d2fb3e788777362d5195d6451899ae
AniruddhUdayan <aniruddhudayan34@gmail.com>
2024-02-08 01:31:18 +0530

    backend authorization

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,53 @@
-var mongoose = require('mongoose');
-//Set up default mongoose connection
-var mongoDB = 'mongodb://localhost:27017';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
- //Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://Ani:Abhiani@atlascluster.phipben.mongodb.net/')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+})
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = {
+    User, Account
+}
+
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,79 +1,153 @@
-const express = require('express')
-const app = express()
-const { z } = require('zod');
-const {User} = require('../db')
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const { z } = require("zod");
+const { User } = require("../db");
+const { Account } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const { authMiddleware } = require("../middleware");
 
-mongoose.connect('mongodb+srv://Ani:Abhiani@atlascluster.phipben.mongodb.net/')
+mongoose.connect("mongodb+srv://Ani:Abhiani@atlascluster.phipben.mongodb.net/");
 
 const schemaSignUp = z.object({
-    username: z.string().email(),
-    firstName: z.string().min(2).max(50),
-    lastName: z.string().min(2).max(50),
-    password: z.string().min(6),
-  });
-const schemaSignIn = z.object({
-    username: z.string().email(),
-    password: z.string().min(6),
-  });
+  username: z.string().email(),
+  firstName: z.string().min(2).max(50),
+  lastName: z.string().min(2).max(50),
+  password: z.string().min(6),
+});
 
-app.post('/signup', async (req,res)=>{
+router.post("/signup", async (req, res) => {
   try {
-    const {username , firstName , lastName , password} = schemaSignUp.safeParse(req.body)
-    if (!username) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        })
+    console.log(req.body);
+    const { success } = schemaSignUp.safeParse(req.body);
+    if (!success) {
+      return res.status(411).json({
+        message: "Incorrect inputs",
+      });
     }
-    const exisitngUser = await User.findOne({username});
-    if(exisitngUser){
-        return res.status(411).send('User already exists')
+    const exisitngUser = await User.findOne({ username: req.body.username });
+    if (exisitngUser) {
+      return res.status(411).send("User already exists");
     }
-    const user = await User.create({username , firstName , lastName , password})
+    const user = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+
     const userId = user._id;
-    
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET);
+
+    await Account.create({
+      userId,
+      balance: 1 + Math.random() * 10000,
+    }); // Giving an initial amount to user on signup
+
+    const token = jwt.sign(
+      {
+        userId,
+      },
+      JWT_SECRET
+    );
 
     res.json({
-        message: "User created successfully",
-        token: token
-    })
-    
+      message: "User created successfully",
+      token: token,
+    });
   } catch (error) {
-    console.error('Validation error:', error.message);
-    
-    // Send error response
-    res.status(400).send('Error while signing in');
-  }
-})
+    console.error("Validation error:", error.message);
 
-app.post('/signin',async (req,res)=>{
-   try {
-    const {username , password} = schemaSignIn.safeParse(req.body);
-    if (!username) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        })
+    // Send error response
+    res.status(400).send("Error while signing in");
+  }
+});
+
+const schemaSignIn = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
+
+router.post("/signin", async (req, res) => {
+  try {
+    const { success } = schemaSignIn.safeParse(req.body);
+    if (!success) {
+      return res.status(411).json({
+        message: "Incorrect inputs",
+      });
     }
-    const exisitngUser = await User.findOne({username});
-    if(!exisitngUser){
-        return res.status(411).send('User does not exist exists')
+    const exisitngUser = await User.findOne({ username:req.body.username, password: req.body.password });
+    if (!exisitngUser) {
+      return res.status(411).send("User does not exist exists");
     }
     const userId = exisitngUser._id;
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET);
+    const token = jwt.sign(
+      {
+        userId,
+      },
+      JWT_SECRET
+    );
 
     res.json({
-        token: token
-    })
-   } catch (error) {
-    console.error('Validation error:', error.message);
-    
+      token: token,
+    });
+  } catch (error) {
+    console.error("Validation error:", error.message);
+
     // Send error response
-    res.status(400).send('Error while logging in');
-   }
-})
+    res.status(400).send("Error while logging in");
+  }
+});
+
+const schemaUserUpdate = z.object({
+  firstName: z.string().min(2).max(50),
+  lastName: z.string().min(2).max(50),
+  password: z.string().min(6),
+});
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { password, firstName, lastName } = schemaUserUpdate.safeParse(
+    req.body
+  );
+  if (!firstName) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+  await User.updateOne(req.body, {
+    _id: req.userId,
+  });
+  res.json({
+    message: "Updated successfully",
+  });
+});
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter, //$regex is used for finding substring in string
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
+module.exports = router;
