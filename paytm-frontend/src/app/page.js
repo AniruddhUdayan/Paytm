1a54d35d302c2aaa7dfc0ab26641c4e433b0ccb8
AniruddhUdayan <aniruddhudayan34@gmail.com>
2024-02-25 19:23:27 +0530

    fix

--- paytm-frontend/src/app/page.js
+++ paytm-frontend/src/app/page.js
@@ -1,20 +1,15 @@
-
-'use client'
+"use client";
 import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    // Check if the token is present in localStorage
     const token = localStorage.getItem("token");
-  
+
     if (token) {
-      // Token is present, fetch the user's name
-      window.location.href = '/dashboard';
-    } else{
-      window.location.href = '/signin';
+      window.location.href = "/dashboard";
+    } else {
+      window.location.href = "/signin";
     }
-  }, []); 
-  return (
-    <div>Hello</div>
-  );
+  }, []);
+  return <div>Hello</div>;
 }
